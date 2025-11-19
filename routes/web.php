1b4244220add4cbd491c03a322ab13ffdd10103f
Octaviofg <48572130+OctavioFloG@users.noreply.github.com>
2025-11-19 15:45:40 -0600

    Add routes to controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\CuentaPorCobrarController;
+use App\Http\Controllers\InstitutionController;
+use App\Http\Controllers\NotificacionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::apiResource('institutions', InstitutionController::class);
+Route::apiResource('users', UserController::class);
+Route::apiResource('clients', ClientController::class);
+Route::apiResource('cuentas-por-cobrar', CuentaPorCobrarController::class);
+Route::apiResource('notificaciones', NotificacionController::class);
